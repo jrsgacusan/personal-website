004013add8110c65c5a9fd5b71fe635647d6dc3a
Juel Rei S. Gacusan <jgacusan@tacitknowledge.com>
2022-05-29 02:11:43 +0800

    Use react-bootstrap card component

--- src/components/Works/List.tsx
+++ src/components/Works/List.tsx
@@ -1,38 +1,38 @@
-import React from 'react';
-import ListItem from './ListItem';
-import classes from './List.module.scss';
-import { Work } from '../../models/Work';
-import bsu from '../../assets/images/bsu-smis.png';
-import quicks from '../../assets/images/quick-s.png';
-import picpa from '../../assets/images/picpa-mis.png';
+import React from "react";
+import ListItem from "./ListItem";
+import classes from "./List.module.scss";
+import { Work } from "../../models/Work";
+import bsu from "../../assets/images/bsu-smis.png";
+import quicks from "../../assets/images/quick-s.png";
+import picpa from "../../assets/images/picpa-mis.png";
 
 const list: Work[] = [
   {
     isPublished: false,
     image: quicks,
-    title: 'Quick Service Android Application',
+    title: "Quick Service Android Application",
     description:
-      'Android mobile application developed using Kotlin. It serves as a service marketplace application for Baguio City Local Citizens.',
-    id: 'a1',
-    href: 'https://github.com/jrsgacusan/Quick-Service',
+      "Android mobile application developed using Kotlin. It serves as a service marketplace application for Baguio City Local Citizens.",
+    id: "a1",
+    href: "https://github.com/jrsgacusan/Quick-Service",
   },
   {
     isPublished: false,
     image: bsu,
-    title: 'BSU SMIS React App',
+    title: "BSU SMIS React App",
     description:
-      'The react version of Benguet State University School Management Information System. It was developed using React.js.',
-    id: 'a2',
-    href: 'https://github.com/jrsgacusan/iol-bsu-smis',
+      "The react version of Benguet State University School Management Information System. It was developed using React.js.",
+    id: "a2",
+    href: "https://github.com/jrsgacusan/iol-bsu-smis",
   },
   {
     isPublished: true,
     image: picpa,
-    title: 'PICPA MIS',
+    title: "PICPA MIS",
     description:
-      'Membership Information System for Philippine Institute of Certified Public Accountant - Benguet Chapter. Front-end technologies used: React.js, Bootstrap, HTML, CSS, and JavaScript.',
-    id: 'a3',
-    href: 'https://picpabbmp.com/',
+      "Membership Information System for Philippine Institute of Certified Public Accountant - Benguet Chapter. Front-end technologies used: React.js, Bootstrap, HTML, CSS, and JavaScript.",
+    id: "a3",
+    href: "https://picpabbmp.com/",
   },
 ];
 
@@ -40,11 +40,7 @@ const List: React.FC = () => {
   return (
     <div className={classes.container}>
       {list.map((item) => {
-        return (
-          <div className={classes.item} key={item.id}>
-            <ListItem work={item} />
-          </div>
-        );
+        return <ListItem work={item} key={item.id} />;
       })}
     </div>
   );
--- src/components/Works/ListItem.tsx
+++ src/components/Works/ListItem.tsx
@@ -1,32 +1,48 @@
-import React from 'react';
-import classes from './ListItem.module.scss';
-import { Work } from '../../models/Work';
+import React from "react";
+import classes from "./List.module.scss";
+import { Work } from "../../models/Work";
+import { Button, Card } from "react-bootstrap";
 
-const ListItem: React.FC<{ work: Work }> = (props) => {
-  const handeOnClick = (e: any) => {
-    window.open(props.work.href, '_blank');
+const ListItem: React.FC<{ work: Work }> = ({
+  work: { href, image, title, description, isPublished },
+}) => {
+  const handeOnClick = () => {
+    window.open(href, "_blank");
   };
 
   return (
-    <div
-      onClick={handeOnClick}
-      className={classes.container}
-      title={props.work.href}
-    >
-      <img
-        className={classes.image}
-        src={props.work.image}
-        alt={props.work.title}
-      />
-      <div className={classes.text}>
-        <h1>{props.work.title}</h1>
-        <p>{props.work.description}</p>
-        <a href={props.work.href} target="__blank">
-          <h1 className={classes.view}>{props.work.isPublished? 'View Website': 'View Code'} </h1>
-        </a>
-      </div>
-    </div>
+    <Card className={classes.card} onClick={handeOnClick}>
+      <Card.Img className={classes.img} src={image} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{description}</Card.Text>
+        <Button variant="primary">
+          {isPublished ? "View Website" : "View Code"}{" "}
+        </Button>
+      </Card.Body>
+    </Card>
   );
+
+  // return (
+  //   <div
+  //     onClick={handeOnClick}
+  //     className={classes.container}
+  //     title={props.work.href}
+  //   >
+  //     <img
+  //       className={classes.image}
+  //       src={props.work.image}
+  //       alt={props.work.title}
+  //     />
+  //     <div className={classes.text}>
+  //       <h1>{props.work.title}</h1>
+  //       <p>{props.work.description}</p>
+  //       <a href={props.work.href} target="__blank">
+  //         <h1 className={classes.view}></h1>
+  //       </a>
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default ListItem;
